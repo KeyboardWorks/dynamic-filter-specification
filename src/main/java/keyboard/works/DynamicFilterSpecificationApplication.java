1aa5acd3223bb57b8f5b2dcf52ce0ff3c9b290ef
Andres Nodas <nodasiniesta@gmail.com>
2021-07-13 16:35:22 +0700

    First Commit

---
+++ src/main/java/keyboard/works/DynamicFilterSpecificationApplication.java
@@ -0,0 +1,13 @@
+package keyboard.works;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicFilterSpecificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicFilterSpecificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/keyboard/works/DynamicFilterSpecificationApplicationTests.java
@@ -0,0 +1,13 @@
+package keyboard.works;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicFilterSpecificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
